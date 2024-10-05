c8e730ab8fa26e6bd933903d111475a5adcce950
dingjiayuan <dingjiayuan13@gmail.com>
2024-10-05 21:30:43 +0800

    solve enums1

--- exercises/enums/enums1.rs
+++ exercises/enums/enums1.rs
@@ -2,19 +2,24 @@
 //
 // No hints this time! ;)
 
-// I AM NOT DONE
 
 
+#[derive(Debug)]
 enum Message {
     Quit,
-    Echo,
-    Move,
-    ChangeColor,// TODO: define a few types of messages as used below
+    Echo(String),
+    Move(i32,i32),
+    ChangeColor(u8,u8,u8),
 }
 
 fn main() {
-    println!("{:?}", Message::Quit);
-    println!("{:?}", Message::Echo);
-    println!("{:?}", Message::Move);
-    println!("{:?}", Message::ChangeColor);
+    let messages = [
+        Message::Quit,
+        Message::Echo("Hello".to_string()),
+        Message::Move (3,4),
+        Message::ChangeColor(255, 0, 0),
+    ];
+    for message in messages.iter() {
+        println!("{:?}",message);
+    }
 }
