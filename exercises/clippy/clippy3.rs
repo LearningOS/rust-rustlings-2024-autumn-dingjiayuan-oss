f0dec51da2f3c7cad8c4248ad7f4e8897b4bfcc6
dingjiayuan <dingjiayuan13@gmail.com>
2024-10-18 11:23:19 +0800

    solve clippy

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -7,13 +7,13 @@
 #[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
-    //if my_option.is_none() {
+    if my_option.is_none() {
     //    println!("my_option is None, cannot unwrap!")
-    //}
+    }
 
     let my_arr = &[
         -1, -2, -3,
-        -4, -5, -6
+        -4, -5, -6,
     ];
     println!("My array! Here it is: {:?}", my_arr);
 
